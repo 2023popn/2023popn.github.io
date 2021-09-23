bf1048676d84ac36537cfa50b7b534c9e58f2592
2023popn <81972529+2023popn@users.noreply.github.com>
2021-09-23 10:58:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-async function getAssignments(){
-    const respNew = await fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments');
-    const txt = await respNew.text();
-    document.write(txt);
 
 document.write("testing123");
