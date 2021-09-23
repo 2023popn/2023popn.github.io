a5f021e4f9d0e4df616c03659f0956aad49e7580
2023popn <81972529+2023popn@users.noreply.github.com>
2021-09-23 14:32:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments')
-      .then(function (response) {
-        switch (response.status) {
-            // status "OK"
-            case 200:
-                return response.text();
-            // status "Not Found"
-            case 404:
-                throw response;
-        }
-    })
-    .then(function (template) {
-        document.write(template);
-    })
-    .catch(function (response) {
-        // "Not Found"
-        console.log(response.statusText);
-    });
-  
+
+function getAssignments(){
+      fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments')
+            .then(function (response) {
+              switch (response.status) {
+                  // status "OK"
+                  case 200:
+                      return response.text();
+                  // status "Not Found"
+                  case 404:
+                      throw response;
+              }
+          })
+          .then(function (template) {
+              document.write(template);
+          })
+          .catch(function (response) {
+              // "Not Found"
+              console.log(response.statusText);
+          });
+}
+
+document.addEventListener("DOMContentLoaded", getAssignments);
